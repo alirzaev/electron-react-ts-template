606163ccb1f99c734d0cb332c46811458299fe67
AliRzaev <alirzaev1996@gmail.com>
2020-04-13 12:03:17 +0300

    Initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../index.css';
+
+export class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <h1>Electron Forge with React and TypeScript template</h1>
+        )
+    }
+}
